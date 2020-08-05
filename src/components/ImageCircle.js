e0c545a2cccb92ca41d60a327e3d3f8f9f4a38c2
Madalena PAVAO <madalenapavao@gmail.com>
2020-08-05 13:49:47 +0200

    ImageCircle component

---
+++ src/components/ImageCircle.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ImageCircle.css';
+
+const ImageCircle = () => {
+    return (
+        <div class="container" > 
+            <img src="https://picsum.photos/id/1014/300/300" />
+            <p>Madalena</p>
+            <p>Age: 25 ans</p>
+            <p>Description:</p>
+        </div>
+    )
+}
+
+export default ImageCircle
--- src/index.js
+++ src/index.js
@@ -4,15 +4,31 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+//FirstCompo et FirstCompo2 font la meme chose
 const FirstCompo = () => {
-  return React.createElement("p", { style :{backgroundColor : 'green'}, id: 'parentP' }, "Nous sommes en formations React !!!!")
+  return React.createElement("p", { style : {backgroundColor : 'green'}, id : 'parentP' }, "Nous sommes en formations React !!!!")
 }
 
+const FirstCompo2 = () => {
+  return <p style={{backgroundColor : 'green'}}>"Nous sommes en formations React !!!!"</p>
+}
+
+const Hello = ({ name }) => React.createElement("h1", {}, `Hello ${ name }`)
+
+function HelloAlten() {
+  return (<div> <Hello name="Alten" /> </div>)
+}
+
+//Affichage du DOM virtuel dans la page
 ReactDOM.render(
-  <FirstCompo />,
+  // <FirstCompo />,
+  // <FirstCompo2 />,
+  // <HelloAlten />,
   // <React.StrictMode>
-  //   <App />
+    <App />,
   // </React.StrictMode>,
+ 
   document.getElementById('root')
 );
 
